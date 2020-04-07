8c4085c1015efc847bb11aacaca10c7805a4b084
GuiDevloper <guicorrea384@gmail.com>
2020-04-07 14:42:48 -0300

    solved API domain

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -9,7 +9,7 @@ export class HeroService {
   constructor(private http: HttpClient) { }
 
   getHero(heroName: string) {
-    return this.http.get('http://gateway.marvel.com/v1/public/characters?name='
+    return this.http.get('https://gateway.marvel.com/v1/public/characters?name='
       + heroName + '&apikey=346b1e472cf83a3fd584aa4136e5d1d5');
   }
 
